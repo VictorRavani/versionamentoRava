e42768c62153bf6b0b4bf7c620975b7a2c4128f4
VictorRavani <victorravani98@gmail.com>
2022-03-28 19:44:58 -0300

    Ativiade 1

---
+++ ATV1.js
@@ -0,0 +1,25 @@
+
+// ----------- Victor Ravani --------------
+
+var n1 = 1;
+var n2 = 2;
+var n3 = 3;
+var n4 = 4;
+var n5 = 5;
+var n6 = 6;
+var n7 = 7;
+var n8 = 8;
+var n9 = 9;
+var n10 = 10;
+
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+console.log(n5);
+console.log(n6);
+console.log(n7);
+console.log(n8);
+console.log(n9);
+console.log(n10);
+
